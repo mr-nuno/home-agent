9bf767fa63566cbfc77b019eb8f2cb58f5834594
Peter Wikström <peter@wikstrom.in>
2014-11-01 15:14:38 +0100

    Initial Commit

---
+++ HomeAgent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeAgent
+//
+//  Created by Peter Wikström on 01/11/14.
+//  Copyright (c) 2014 Peter Wikström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
